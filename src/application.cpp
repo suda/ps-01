8e18cf3454b2cf017275c7e7e6e3b82a174b5f6e
Wojtek Siudzinski <admin@suda.pl>
2016-06-12 00:13:14 +0100

    Support Ctrl+C

--- src/application.cpp
+++ src/application.cpp
@@ -2,7 +2,11 @@
 #include "application.h"
 #else
 #include <stdio.h>
-#include <stdint.h>
+#include <sys/signal.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
 #endif
 
 #include "synth/synth.h"
@@ -16,9 +20,28 @@ void setup() {
 void loop() {}
 
 #if !defined(PARTICLE)
+void exit_handler(int s){
+    printf("(i) exiting\n");
+    exit(1);
+
+}
+
 int main(int argc, char **argv) {
-	printf("ps-01 starting...\n");
+	printf("\033[1;31m~~~ ps-01 synthesizer ~~~\033[0m\n\n");
 	setup();
-	printf("--> running...\n");
+	printf("(i) running...\n");
+
+    // Stopping on Ctrl+C
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = exit_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    pause();
+
+    return 0;
 }
 #endif
