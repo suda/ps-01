7c3b885d8812d2ed30b361b2f720943ab2474a16
Wojtek Siudzinski <admin@suda.pl>
2019-11-03 01:30:09 +0100

    Add line drawing implementation

--- lib/Adafruit_SDL/Adafruit_SDL.cpp
+++ lib/Adafruit_SDL/Adafruit_SDL.cpp
@@ -32,27 +32,43 @@ void Adafruit_SDL::end() {
     SDL_DestroyWindow(window);
 }
 
+Uint8* rgb565to888(uint16_t color) {
+    Uint8 *rgb = new Uint8[3];
+    rgb[0] = (color & 0xF800) >> 8;
+    rgb[1] = (color & 0x07E0) >> 3;
+    rgb[2] = (color & 0x1F) << 3;
+
+    return rgb;
+}
+
 void Adafruit_SDL::drawPixel(int16_t x, int16_t y, uint16_t color) {
-    Uint8 r = (color & 0xF800) >> 8;
-    Uint8 g = (color & 0x07E0) >> 3;
-    Uint8 b = (color & 0x1F) << 3;
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-    SDL_RenderDrawPoint(renderer, x * SCALE, x * SCALE);
+    Uint8 *rgb = rgb565to888(color);
+    SDL_SetRenderDrawColor(renderer, rgb[0], rgb[1], rgb[2], 255);
+    SDL_RenderDrawPoint(renderer, x * SCALE, y * SCALE);
+    free(rgb);
 }
 void Adafruit_SDL::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color) {
-    Uint8 r = (color & 0xF800) >> 8;
-    Uint8 g = (color & 0x07E0) >> 3;
-    Uint8 b = (color & 0x1F) << 3;
+    Uint8 *rgb = rgb565to888(color);
     SDL_Rect rect;
     rect.x = x * SCALE;
     rect.y = y * SCALE;
     rect.w = w * SCALE;
     rect.h = h * SCALE;
     
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+    SDL_SetRenderDrawColor(renderer, rgb[0], rgb[1], rgb[2], 255);
     SDL_RenderFillRect(renderer, &rect);
-
+    free(rgb);
 }
-void Adafruit_SDL::drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color) {}
-void Adafruit_SDL::drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color) {}
+void Adafruit_SDL::drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color) {
+    Uint8 *rgb = rgb565to888(color);
+    SDL_SetRenderDrawColor(renderer, rgb[0], rgb[1], rgb[2], 255);
+    SDL_RenderDrawLine(renderer, x * SCALE, y * SCALE, (x + w - 1) * SCALE, y * SCALE);
+    free(rgb);
+}
+void Adafruit_SDL::drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color) {
+    Uint8 *rgb = rgb565to888(color);
+    SDL_SetRenderDrawColor(renderer, rgb[0], rgb[1], rgb[2], 255);
+    SDL_RenderDrawLine(renderer, x * SCALE, y * SCALE, x * SCALE, (y + h - 1) * SCALE);
+    free(rgb);
+}
 #endif
